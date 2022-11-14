f0b29f74b781ee3352060eee3841726989d78070
ducngtrung <ducnguyen.bme@gmail.com>
2022-11-14 14:21:15 +0700

    Spring boot day02

--- 5_SpringBoot/Day02_REST_CRUD/BTVN_Day02_REST_CRUD/src/main/java/techmaster/LoginFeatureApplication.java
+++ 5_SpringBoot/Day02_REST_CRUD/BTVN_Day02_REST_CRUD/src/main/java/techmaster/LoginFeatureApplication.java
@@ -3,6 +3,29 @@ package techmaster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//1. Hãy định nghĩa một class User gồm các trường:
+//
+//        id : Kiểu int
+//        username : Kiểu String
+//        email : Kiểu String
+//        password : Kiểu String
+//        avatar : kiểu String
+//
+//        Với các phương thức như : setter, getter, constructor, … sử dụng Lombok để thực hiện
+//
+//2. Trong ứng dụng Spring Boot tạo API thực hiện chức năng sau:
+//
+//   POST http://localhost:8080/login
+//   Reqest Body có cấu trúc như sau:
+//    {
+//        "username" : username
+//        "password" : password
+//    }
+//
+//   Kết quả trả về:
+//   Nếu username và password chính xác : Trả về thông tin của user bao gồm username, email, avatar
+//   Nếu username và password không chính xác : Trả về username hoặc password chưa chính xác
+
 @SpringBootApplication
 public class LoginFeatureApplication {
 
