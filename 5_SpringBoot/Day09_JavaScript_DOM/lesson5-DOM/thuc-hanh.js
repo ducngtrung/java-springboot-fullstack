3bfcf8492cef6d11e1742612d1eaaabaac90e7e1
ducngtrung <ducnguyen.bme@gmail.com>
2022-11-30 19:58:28 +0700

    js

--- 5_SpringBoot/Day09_JavaScript_DOM/lesson5-DOM/thuc-hanh.js
+++ 5_SpringBoot/Day09_JavaScript_DOM/lesson5-DOM/thuc-hanh.js
@@ -7,12 +7,10 @@ document.getElementsByClassName('text-content')[0].querySelectorAll('p')[1]
 
 
 // Bài 3: In ra cửa sổ console các thẻ li có trong list. Đổi kết quả về dạng mảng và in ra cửa sổ console.
-liTags = document.querySelector('ul').querySelectorAll('li')
-liArray = [...liTags]
-console.log(liArray)
+Array.from(document.querySelector('ul').querySelectorAll('li'))
 
 
-// Bài 4: In ra cửa số console các thẻ p trong thẻ div có class là “text-content”. Đổi kết quả về dạng mảng và in ra cửa sổ console (Gợi ý, sử dụng spread operator).
+// Bài 4: In ra cửa số console các thẻ p trong thẻ div có class là “text-content”. Đổi kết quả về dạng mảng và in ra cửa sổ console (sử dụng spread operator).
 pTags = document.getElementsByClassName('text-content')[0].querySelectorAll('p')
 pArray = [...pTags]
 console.log(pArray)
