b03eafd20dde59446d2dc01203ceef5c64713e01
ducngtrung <ducnguyen.bme@gmail.com>
2022-12-11 00:10:30 +0700

    kiem-tra-giua-khoa-JS

--- 5_SpringBoot/Day12_JavaScript_KiemTraGiuaKhoa/main.js
+++ 5_SpringBoot/Day12_JavaScript_KiemTraGiuaKhoa/main.js
@@ -1,30 +1,29 @@
-// Tạo 1 ứng dụng xem thời tiết đơn giản bằng javascript
+// Tạo ứng dụng xem thời tiết đơn giản bằng javascript
 
-// 1. Tạo 1 trang HTML có các yêu cầu sau:
+// 1. Tạo trang HTML với các yêu cầu sau:
 
 //     1 input nhập vào tên của thành phố muốn xem thời tiết
-//     1 Button submit
-//     1 Button thêm mới để nhập thêm 1 thành phố mới muốn xem thời tiết (Tối đa 3 thành phố)
-//     1 Button xoá để remove thành phố
-//     1 Button refresh để reload lại trạng thái thời tiết. (Các icon của button có thể dung các icon sau https://heroicons.com/)
-//     Khung hiển thị thông tin thời tiết, có đầy đủ các thông tin sau: Thời gian hiện tại, ngày tháng, tên thành phố, nhiệt độ hiện tại, icon mô tả thời tiết, độ ẩm, thời gian mặt trời mọc, thời gian mặt trời lặn.
-//     Tham khảo hình sau (Thiết kế tuỳ ý, ko bắt buộc theo mẫu) hoặc tham khảo một số mẫu sau:
+//     1 button thêm mới để nhập thêm 1 thành phố mới muốn xem thời tiết (tối đa 3 thành phố)
+//     1 button xoá để remove thành phố
+//     (Các icon của button có thể dùng bộ icon sau https://heroicons.com/)
+//     Khung hiển thị thông tin thời tiết, có đầy đủ các thông tin sau: Thời gian hiện tại, ngày tháng, tên thành phố, nhiệt độ hiện tại, icon mô tả thời tiết, độ ẩm, thời gian mặt trời mọc, thời gian mặt trời lặn. Tham khảo một số mẫu sau:
 //         https://codepen.io/thomvallez/pen/NqyqWV
 //         https://codepen.io/thecodingaviator/pen/BqBMYP
 
 // 2. Sử dụng API weather để lấy thông tin:
 
 //     Tạo tài khoản để lấy API key và hướng dẫn sử dụng api tại: https://www.weatherbit.io/
-//     Document chi tiết API: https://www.weatherbit.io/api/weather-current (Xem kỹ)
-//     Danh sách icon: https://www.weatherbit.io/api/codes (Lưu ý cần hiển thị icon theo ngày và đêm chính xác theo giờ hiện tại)
+//     Document chi tiết của API: https://www.weatherbit.io/api/weather-current
+//     Danh sách icon mô tả thời tiết: https://www.weatherbit.io/api/codes
 
 
 const searchInput = document.getElementById("search-field");
 const submitBtn = document.getElementById('submit-button');
 const removeBtn = document.getElementById('remove-cities-button');
 const apiKey = "d9e0abf6205f408db0e3978faa96c84a";
-var result;
 var count = 0;
+var result;
+
 
 // Hàm gọi API để lấy dữ liệu thời tiết mới nhất
 function getWeatherData() {
@@ -36,16 +35,14 @@ function getWeatherData() {
         .then(function(res) {
             result = res.data.data[0];
             ++count;
-            while (count <= 3) {
+            if (count <= 3) {
                 renderData(result);
-                return;
             }
         })
         .catch(error => {
             console.log(error);
         })
 }
-
 
 function renderData(result) {
     document.getElementById(`card-${count}`).getElementsByClassName('city')[0].innerHTML = result.city_name;
@@ -69,7 +66,7 @@ searchInput.addEventListener("keypress", function(event) {
   }
 });
 
-// Lắng nghe sự kiện khi click vào nút "Submit" để add city
+// Lắng nghe sự kiện khi click vào nút "Add city"
 submitBtn.addEventListener("click", function () {
     getWeatherData();
 });
